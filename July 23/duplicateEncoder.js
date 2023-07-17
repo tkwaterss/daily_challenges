24d425dd0f37fd11417915a860d769030a3f026b
Tobin Waters <tkwaterss@gmail.com>
2023-07-17 12:56:09 -0600

    Duplicate Letter Encoder Solution, could use some refinement

---
+++ July 23/duplicateEncoder.js
@@ -0,0 +1,32 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+let test1 = "Success"
+
+
+const duplicateEncode = (string) => {
+  string = string.toLowerCase().split("");
+  for (let i = 0; i < string.length; i++) {
+    let matches = string.filter(letter => {
+      return letter === string[i]
+    })
+    if (matches.length > 1) {
+      string = string.map(letter => {
+        if (letter === string[i]) {
+          return ')'
+        } else {
+          return letter;
+        }
+      })
+    }
+    //if not equal to multiple ( then change to )
+  }
+  return string.map(letter => {
+    if (letter !== ')') {
+      return '(';
+    } else {
+      return letter
+    }
+  }).join('');
+};
+
+console.log(duplicateEncode(test1));
